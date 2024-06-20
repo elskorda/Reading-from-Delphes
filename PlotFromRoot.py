0d0cff392019586c68a3feea8d90b558171beaed
Karol Krizka <kkrizka@gmail.com>
2024-06-20 21:21:47 +0100

    Add configuration using runconfigs.

--- PlotFromRoot.py
+++ PlotFromRoot.py
@@ -1,9 +1,12 @@
 import ROOT
 import argparse
 
+from analysis import config
 from analysis import Sample
 from analysis import ExampleAnalysis
-           
+
+from kkconfig import runconfig
+
 def plot_and_save(*args):
     """
     Creates a THStack to overlay the histograms inside `*args, draws the
@@ -42,42 +45,47 @@ def plot_and_save(*args):
         # Save the canvas as a PNG file
         canvas.SaveAs(f'{hname}.png')
 
-def main(signal_file, background_file):
+def main(runconfig_path):
     """
     Main function to read tree contents, create distributions, and plot/save 
     the histograms.
     """
 
-    signal=Sample.Sample('Signal')
-    signal.add_file(signal_file)
-    signal.open()
+    runcfg=runconfig.load([runconfig_path])
 
-    background=Sample.Sample('Background')
-    background.add_file(background_file)
-    background.open()
+    samples=[]
+    for input in runcfg.get('inputs',[]):
+
+        sample=Sample.Sample(input.get('title',''))
+
+        path=input.get('path',[])
+        if type(path) is not list:
+            path=[path]
+
+        for p in path:
+            sample.add_file(p)
+        sample.open()
+
+        samples.append(sample)
 
     analysis=ExampleAnalysis.ExampleAnalysis()
 
-    sig_hists=analysis.run(signal)
-    bkg_hists=analysis.run(background)
+    hists=map(analysis.run, samples)
 
-    plot_and_save(sig_hists, bkg_hists)
+    plot_and_save(*list(hists))
 
 if __name__ == "__main__":
     # Set up argument parser
     parser = argparse.ArgumentParser(description="script that reads root files to create and plot distributions.")
-    parser.add_argument("--signal_file",default="uC_ww_leplep_sqrts01000_mw80419.root", type=str, help="Path to the signal ROOT file.")
-    parser.add_argument("--background_file", default="uC_ww_leplep_sqrts01000_mw80419.root", type=str, help="Path to the background ROOT file.")
+    parser.add_argument("runconfig", type=str, help="Path to the run configuration YAML file.")
 
-    mg5amcnlo = '/home/kkrizka/Sources/mg5amcnlo'
+    ROOT.gInterpreter.AddIncludePath(f"{config.mg5amcnlo}/Delphes");
+    ROOT.gInterpreter.AddIncludePath(f"{config.mg5amcnlo}/Delphes/external");
 
-    ROOT.gInterpreter.AddIncludePath(f"{mg5amcnlo}/Delphes");
-    ROOT.gInterpreter.AddIncludePath(f"{mg5amcnlo}/Delphes/external");
-
-    ROOT.gSystem.Load(f'{mg5amcnlo}/Delphes/libDelphes.so')
+    ROOT.gSystem.Load(f'{config.mg5amcnlo}/Delphes/libDelphes.so')
     
     # Parse the arguments
     args = parser.parse_args()
 
     # Execute the main function with the provided arguments
-    main(args.signal_file, args.background_file)
+    main(args.runconfig)
---
+++ analysis/config.py
@@ -0,0 +1,5 @@
+from kkconfig import local
+
+mg5amcnlo = '/home/kkrizka/Sources/mg5amcnlo'
+
+local.load_settings('.analysis.yaml', globals())
